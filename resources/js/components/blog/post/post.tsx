b87c66526ebd92c28d892082df17c2d7daa777da
Frusadev <frusadev@gmail.com>
2025-04-19 22:20:25 +0000

    Improve post look

--- resources/js/components/blog/post/post.tsx
+++ resources/js/components/blog/post/post.tsx
@@ -1,35 +1,37 @@
 import { Post } from "@/types/post";
 import { Link } from "@inertiajs/react";
-import { TableProperties } from "lucide-react";
+import { ChevronRight, TableProperties } from "lucide-react";
 
 export default function PostComponent({ post }: { post: Post }) {
   return (
-    <article className="motion-preset-bounce bg-card mb-4 flex items-center justify-center border p-4 shadow">
+    <article className="motion-preset-bounce bg-card mb-4 flex items-center justify-center border p-4 shadow rounded-xl">
       <Link
         href={route("blog.show", { slug: post.slug, post: post.id })}
         className="flex w-full cursor-default flex-col items-start justify-between gap-4 md:flex-row"
       >
         <div className="w-full md:w-1/2">
           <img
-            className="h-60 w-full cursor-pointer object-cover"
+            className="h-60 w-full cursor-pointer object-cover rounded-xl"
             src={
               post.image ? `/storage/${post.image}` : "/assets/img/cover.png"
             }
             alt={post.title}
           />
         </div>
-        <div className="flex w-full flex-col justify-between md:w-1/2">
+        <div className="flex w-full flex-col justify-between md:w-1/2 gap-4">
           <div>
             <h4 className="tex-3xl text-primary mb-2 flex items-center gap-2 font-medium">
-              <TableProperties />
+              <ChevronRight />
               {post.category.name}
             </h4>
-            <h3 className="cursor-pointer text-2xl font-semibold">
-              {post.title}
-            </h3>
-            <p className="text-muted-foreground mb-2 text-sm">
-              {post.description}
-            </p>
+            <div className="flex flex-col gap-4">
+              <h3 className="cursor-pointer text-2xl font-semibold">
+                {post.title}
+              </h3>
+              <p className="text-muted-foreground mb-2 text-sm">
+                {post.description}
+              </p>
+            </div>
           </div>
           <div className="my-2 flex items-start gap-4">
             <img
