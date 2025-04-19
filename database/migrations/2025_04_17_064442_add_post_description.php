1d788ae51877de112db28fedbd8135d6a1cf1020
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-04-19 20:48:27 +0000

    fix: category & post migrations

--- database/migrations/2025_04_17_064442_add_post_description.php
+++ database/migrations/2025_04_17_064442_add_post_description.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table("posts", function (Blueprint $table) {
-            $table->string("description")->nullable()->after("title");
+            $table->string("description")->after("title");
         });
     }
 
