2ad85644222b8fb7161af7e4b9d357dfcf9d7316
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-05-07 08:49:51 +0000

    feat: implement product images

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'description', 'price', 'image', 'author_id', 'stock'];
+    protected $fillable = ['name', 'description', 'price', 'author_id', 'stock'];
 
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
     }
 }
