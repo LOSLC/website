106ca3fc7a828f117f69a6b58c0d3b127ef77999
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-05-07 07:26:16 +0000

    feat(admin): implement product policies and model updates

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['name', 'description', 'price', 'image', 'author_id', 'stock'];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
