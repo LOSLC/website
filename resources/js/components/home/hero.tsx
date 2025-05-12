89bc7bdddbb410dcebea3798133a9a5395599955
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-05-12 18:24:01 +0000

    fix lint errors

--- resources/js/components/home/hero.tsx
+++
@@ -1,9 +0,0 @@
-import { Terminal } from "lucide-react";
-import HeroCard from "./hero-card";
-
-export default function Hero() {
-  return (
-    <div className="flex flex-wrap mt-20 gap-4">
-    </div>
-  );
-}
